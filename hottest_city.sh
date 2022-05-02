3f1d224233cb76957d6943e96dc7d190ceb46014
Itay <itayadler13@gmail.com>
2022-05-02 20:11:25 +0300

    Finished second part, need to QA

--- hottest_city.sh
+++ hottest_city.sh
@@ -1,5 +1,48 @@
 #! /bin/bash
-test=$(./GetCurrentWeather/bin/Debug/net6.0/GetCurrentWeather -c London -u metric)
-IFS='|' read -r -a array <<< "$test"
-temp=${array[1]}
-echo $temp
+csharpApp=$GET_CURRENT_WEATHER_BIN
+filename=$(readlink /proc/self/fd/0)
+mapfile -t arrayOfCities
+while IFS= read -r line; do 
+    arrayOfCities+= $line; 
+done
+< $filename
+arrayOfTemps=()
+arrayOfApprovedCities=()
+len=${arrayOfCities[@]}
+for (( i=0; i<${#arrayOfCities[@]}; i++ )); do
+    test=$(${csharpApp} -c ${arrayOfCities[$i]} &)
+    if [[ -z $test ]]; then
+        echo "Could not pull the weather info for ${arrayOfCities[$i]}"
+    else
+        IFS='|' read -r -a output <<< "$test"
+        temp=${output[1]}
+        arrayOfTemps+=($temp)
+        arrayOfApprovedCities+=("${arrayOfCities[$i]}")
+    fi
+done
+wait
+first=-999
+second=-999
+third=-999
+index1=0
+index2=0
+index3=0
+for (( i=0; i<${#arrayOfTemps[@]}; i++ )); do
+    if [[ ${arrayOfTemps[$i]} -gt $first ]]; then
+        third=$second
+        second=$first
+        first=${arrayOfTemps[$i]}
+        index1=$i
+    elif [[ ${arrayOfTemps[$i]} -gt $second ]]; then
+        third=$second
+        second=${arrayOfTemps[$i]}
+        index2=$i
+    elif [[ ${arrayOfTemps[$i]} -gt $third ]]; then
+        third=${arrayOfTemps[$i]}
+        index3=$i
+    fi
+done
+echo "Three hottest cities are:"
+echo "1. ${arrayOfApprovedCities[$index1]} (${first}C)"
+echo "2. ${arrayOfApprovedCities[$index2]} (${second}C)"
+echo "3. ${arrayOfApprovedCities[$index3]} (${third}C)"
